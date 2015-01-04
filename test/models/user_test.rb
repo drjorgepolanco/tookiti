fcfc9b5664170fdd98a1772ec8d6f22c977424eb
JORGE POLANCO <DR.JORGEPOLANCO@GMAIL.COM>
2015-01-04 04:48:48 -0500

    Email should be unique

--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -57,4 +57,11 @@ class UserTest < ActiveSupport::TestCase
 			assert_not @user.valid?, "#{email.inspect} should not be valid"
 		end
 	end	
+
+	test "email address should be unique" do
+		duplicate_user = @user.dup
+		duplicate_user.email = @user.email.upcase
+		@user.save
+		assert_not duplicate_user.valid?
+	end
 end
