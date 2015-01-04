7d173f07592510d84ed78fc6b72b0e89bac4dccd
JORGE POLANCO <DR.JORGEPOLANCO@GMAIL.COM>
2015-01-04 05:30:02 -0500

    Add resources to Users

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,8 @@
 Rails.application.routes.draw do
   root 'pages#home'
-  get 'about'   => 'pages#about'
-  get 'help'    => 'pages#help'
-  get 'contact' => 'pages#contact'
-  get 'signup'  => 'users#new'
+  get  'about'   => 'pages#about'
+  get  'help'    => 'pages#help'
+  get  'contact' => 'pages#contact'
+  get  'signup'  => 'users#new'
+  resources :users
 end
