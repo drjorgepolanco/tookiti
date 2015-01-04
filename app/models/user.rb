6f4df48b927b62b68e871bad65cf799a35723cff
JORGE POLANCO <DR.JORGEPOLANCO@GMAIL.COM>
2015-01-04 03:57:05 -0500

    Add length validation to name and email

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,3 +1,5 @@
 class User < ActiveRecord::Base
-	validates :first_name, :last_name, :email, presence: true
+	validates :first_name, presence: true, length: { maximum: 30 }
+	validates :last_name,  presence: true, length: { maximum: 30 }
+	validates :email, 		 presence: true, length: { maximum: 100 }
 end
