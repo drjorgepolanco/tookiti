0c6dcdd2c5dcd3405df51ea128e4e50cefcb063e
JORGE POLANCO <DR.JORGEPOLANCO@GMAIL.COM>
2015-01-04 05:15:40 -0500

    Add minimum length validation to email

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,7 +4,7 @@ class User < ActiveRecord::Base
 	validates :last_name,  presence: true, length: { maximum: 30 }
 	
 	VALID_EMAIL = /\A[\w+\-.]+@[a-z\d\-]+(\.[a-z\d\-]+)*\.[a-z]+\z/i
-	validates :email, 		 presence: true, length: { maximum: 100 },
+	validates :email, 		 presence: true, length: { minimum: 7, maximum: 100 },
 												 format: { with: VALID_EMAIL },
 												 uniqueness: { case_sensitive: false }
 	has_secure_password
