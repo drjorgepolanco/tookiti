f5f98e4c471cf162e7ee9276b8e1573760d41125
JORGE POLANCO <DR.JORGEPOLANCO@GMAIL.COM>
2015-01-04 10:26:31 -0500

    Rearrange methods, partially develop create(), create user_params()

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,9 +1,24 @@
 class UsersController < ApplicationController
+  def show
+  	@user = User.find(params[:id])
+  end
+
   def new
   	@user = User.new
   end
 
-  def show
-  	@user = User.find(params[:id])
+  def create
+  	@user = User.new(user_params	)
+  	if @user.save
+  		#
+  	else
+  		render 'new'
+  	end
   end
+
+  private
+
+  	def user_params
+  		params.require(:users).permit(:first_name, :last_name, :email, :password, 
+  																	:password_confirmation)
 end
