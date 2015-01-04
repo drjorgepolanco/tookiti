b4bdec4be38ffd0c51537811b69a051443a7e9b1
JORGE POLANCO <DR.JORGEPOLANCO@GMAIL.COM>
2015-01-04 04:53:18 -0500

    Add index to users email to enforce email uniqueness in database level

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150104083025) do
+ActiveRecord::Schema.define(version: 20150104095142) do
 
   create_table "users", force: true do |t|
     t.string   "first_name"
@@ -20,5 +20,7 @@ ActiveRecord::Schema.define(version: 20150104083025) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
+
+  add_index "users", ["email"], name: "index_users_on_email", unique: true
 
 end
