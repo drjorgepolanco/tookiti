b2e6adb50ef744f44c1e92e25201a7be2c1cd900
JORGE POLANCO <DR.JORGEPOLANCO@GMAIL.COM>
2015-01-04 04:44:57 -0500

    Add email format validation

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,8 @@
 class User < ActiveRecord::Base
 	validates :first_name, presence: true, length: { maximum: 30 }
 	validates :last_name,  presence: true, length: { maximum: 30 }
-	validates :email, 		 presence: true, length: { maximum: 100 }
+	
+	VALID_EMAIL = /\A[\w+\-.]+@[a-z\d\-]+(\.[a-z\d\-]+)*\.[a-z]+\z/i
+	validates :email, 		 presence: true, length: { maximum: 100 },
+												 format: { with: VALID_EMAIL }
 end
