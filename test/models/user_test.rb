c525ee894f32bfaed9d4a4a0ad746d5140f2500e
JORGE POLANCO <DR.JORGEPOLANCO@GMAIL.COM>
2015-01-04 05:26:47 -0500

    Test for email downcasing

--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -64,11 +64,18 @@ class UserTest < ActiveSupport::TestCase
 		end
 	end	
 
-	test "email address should be unique" do
+	test "email should be unique" do
 		duplicate_user = @user.dup
 		duplicate_user.email = @user.email.upcase
 		@user.save
 		assert_not duplicate_user.valid?
+	end
+
+	test "email should be saved as lower case" do
+		mixed_case_email = "uSeR@eXamPLE.coM"
+		@user.email = mixed_case_email
+		@user.save
+		assert_equal mixed_case_email.downcase, @user.reload.email
 	end
 
 	test "password should not be too short" do
