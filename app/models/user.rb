305ae78cb18ad30b23147f84d023fc0dd0c6d1c8
JORGE POLANCO <DR.JORGEPOLANCO@GMAIL.COM>
2015-01-04 04:51:16 -0500

    Add uniqueness validation to email with case sensitivity and downcase before save to avoid duplications at database level

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,8 +1,10 @@
 class User < ActiveRecord::Base
+	before_save { email.downcase! }
 	validates :first_name, presence: true, length: { maximum: 30 }
 	validates :last_name,  presence: true, length: { maximum: 30 }
 	
 	VALID_EMAIL = /\A[\w+\-.]+@[a-z\d\-]+(\.[a-z\d\-]+)*\.[a-z]+\z/i
 	validates :email, 		 presence: true, length: { maximum: 100 },
-												 format: { with: VALID_EMAIL }
+												 format: { with: VALID_EMAIL },
+												 uniqueness: { case_sensitive: false }
 end
