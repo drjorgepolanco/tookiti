6cd6df597ac39e5903d8d1b24f7fa43b67bdd309
JORGE POLANCO <DR.JORGEPOLANCO@GMAIL.COM>
2015-01-05 06:48:57 -0500

    Verifies that signup link dissapears from the header

--- test/integration/users_login_test.rb
+++ test/integration/users_login_test.rb
@@ -22,8 +22,9 @@ class UsersLoginTest < ActionDispatch::IntegrationTest
 		follow_redirect!
 		assert_template 'users/show'
 		assert_select "a[href=?]", login_path, count: 0
+		assert_select "a[href=?]", signup_path, count: 0
 		assert_select "a[href=?]", logout_path
 		assert_select "a[href=?]", user_path(@user)
 	end
-	
+
 end
