b30087138f0e0a23549d44c71bce9517646b720b
JORGE POLANCO <DR.JORGEPOLANCO@GMAIL.COM>
2015-01-05 03:20:20 -0500

    Add test for valid signup

--- test/integration/users_signup_test.rb
+++ test/integration/users_signup_test.rb
@@ -3,12 +3,26 @@ require 'test_helper'
 class UsersSignupTest < ActionDispatch::IntegrationTest
 	test "invalid signup info" do
 		get signup_path
-		assert_no_difference 'User.count' do
-			post users_path, user: { name: "",
-															 email: "user@example.com",
-															 password: "pass",
-															 password_confirmation: "word" }
+		assert_no_difference('User.count') do
+			post users_path, user: { first_name:            '',
+                               last_name:             'Chikinki',
+                               email: 		            'a.chikinki@mail.com',
+                               password:              'pass',
+                               password_confirmation: 'word' }
 		end
 		assert_template 'users/new'
+		assert_select 'div.alert-box'
+	end
+
+	test "valid signup info" do
+		get signup_path
+		assert_difference('User.count', 1) do
+			post_via_redirect users_path, user: { first_name: 'Julito',
+                                            last_name:  'Triculi',
+                                            email:      'triculito@mail.com',
+                                            password:              'password',
+                                            password_confirmation: 'password' }
+		end
+		assert_template 'users/show'
 	end
 end
