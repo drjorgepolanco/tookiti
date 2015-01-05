add1774ebfcc67637c8ac2e89c5de0935226d90f
JORGE POLANCO <DR.JORGEPOLANCO@GMAIL.COM>
2015-01-05 16:36:51 -0500

    Switch to haml, improve layout, make links downcase, create partial fields

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -12,7 +12,7 @@ module ApplicationHelper
 		"#{@user.first_name.capitalize} #{@user.last_name.capitalize}"
 	end
 
-	def gravatar(user, options = {size: 80})
+	def gravatar(user, options = {size: 240})
 		gravatar_id = Digest::MD5::hexdigest(user.email.downcase)
 		size = options[:size]
 		gravatar_url = "https://secure.gravatar.com/avatar/#{gravatar_id}?s=#{size}"
