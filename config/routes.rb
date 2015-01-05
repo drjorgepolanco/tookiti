3f1885add23808a2e53c87273e7368405930f8c2
JORGE POLANCO <DR.JORGEPOLANCO@GMAIL.COM>
2015-01-05 04:15:44 -0500

    Add routes for login and logout

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,11 @@
 Rails.application.routes.draw do
-  get 'sessions/new'
-
-  root 'pages#home'
-  get  'about'   => 'pages#about'
-  get  'help'    => 'pages#help'
-  get  'contact' => 'pages#contact'
-  get  'signup'  => 'users#new'
+  root   'pages#home'
+  get    'about'   => 'pages#about'
+  get    'help'    => 'pages#help'
+  get    'contact' => 'pages#contact'
+  get    'signup'  => 'users#new'
+  get    'login'   => 'sessions#new'
+  post   'login'   => 'sessions#create'
+  delete 'logout'  => 'sessions#destroy'
   resources :users
 end
