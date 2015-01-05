5b6cb8ef7fe3dc620cb192f2c4fc63bd331d470f
JORGE POLANCO <DR.JORGEPOLANCO@GMAIL.COM>
2015-01-05 07:18:27 -0500

    log in with valid info and log out

--- test/integration/users_login_test.rb
+++ test/integration/users_login_test.rb
@@ -6,25 +6,46 @@ class UsersLoginTest < ActionDispatch::IntegrationTest
 		@user = users(:triculito)
 	end
 
-	test "login with invalid information" do
-		get login_path
-		assert_template 'sessions/new'
-		post login_path, session: { email: "", password: "password" }
-		assert_template 'sessions/new'
-		assert_not flash.empty?
-		get root_path
-		assert flash.empty?
+	test "log in with invalid info" do
+		get(login_path)
+		assert_template('sessions/new')
+		post(login_path, session: { email: "", password: "password" })
+		assert_template('sessions/new')
+		assert_not(flash.empty?)
+		get(root_path)
+		assert(flash.empty?)
 	end
 
-	test "login with valid information" do
-		post login_path, session: { email: @user.email, password: 'password' }
-		assert_redirected_to @user
+	test "log in with valid info" do
+		post(login_path, session: { email: @user.email, password: 'password' })
+		assert_redirected_to(@user)
 		follow_redirect!
-		assert_template 'users/show'
-		assert_select "a[href=?]", login_path, count: 0
-		assert_select "a[href=?]", signup_path, count: 0
-		assert_select "a[href=?]", logout_path
-		assert_select "a[href=?]", user_path(@user)
+		assert_template('users/show')
+		assert_select("a[href=?]", login_path,  count: 0)
+		assert_select("a[href=?]", signup_path, count: 0)
+		assert_select("a[href=?]", logout_path)
+		assert_select("a[href=?]", user_path(@user))
 	end
 
+
+	test "log in with valid info and log out" do
+		get(login_path)
+		post(login_path, session: { email: @user.email, password: 'password' })
+		assert(is_logged_in?)
+		assert_redirected_to(@user)
+		follow_redirect!
+		assert_template('users/show')
+		assert_select("a[href=?]", login_path,  count: 0)
+		assert_select("a[href=?]", signup_path, count: 0)
+		assert_select("a[href=?]", logout_path)
+		assert_select("a[href=?]", user_path(@user))
+		delete(logout_path)
+		assert_not(is_logged_in?)
+		assert_redirected_to(root_url)
+		follow_redirect!
+		assert_select("a[href=?]", login_path)
+		assert_select("a[href=?]", signup_path)
+		assert_select("a[href=?]", logout_path,      count: 0)
+		assert_select("a[href=?]", user_path(@user), count: 0)
+	end
 end
--- test/integration/users_signup_test.rb
+++ test/integration/users_signup_test.rb
@@ -1,8 +1,8 @@
 require 'test_helper'
 
 class UsersSignupTest < ActionDispatch::IntegrationTest
-	test "invalid signup info" do
-		get signup_path
+	test "invalid sign up info" do
+		get(signup_path)
 		assert_no_difference('User.count') do
 			post users_path, user: { first_name:            '',
                                last_name:             'Chikinki',
@@ -10,12 +10,12 @@ class UsersSignupTest < ActionDispatch::IntegrationTest
                                password:              'pass',
                                password_confirmation: 'word' }
 		end
-		assert_template 'users/new'
-		assert_select 'div.alert-box'
+		assert_template('users/new')
+		assert_select('div.alert-box')
 	end
 
-	test "valid signup info" do
-		get signup_path
+	test "valid sign up info" do
+		get(signup_path)
 		assert_difference('User.count', 1) do
 			post_via_redirect users_path, user: { first_name: 'Julito',
                                             last_name:  'Triculi',
@@ -23,8 +23,8 @@ class UsersSignupTest < ActionDispatch::IntegrationTest
                                             password:              'password',
                                             password_confirmation: 'password' }
 		end
-		assert_template 'users/show'
-		assert_not flash.empty?
-		assert is_logged_in?
+		assert_template('users/show')
+		assert_not(flash.empty?)
+		assert(is_logged_in?)
 	end
 end
