2429c59d31a5406bfa506f2b0bbf7e3860c338bb
JORGE POLANCO <DR.JORGEPOLANCO@GMAIL.COM>
2015-01-06 00:35:47 -0500

    Create method redirect_back_or() and store_location() to redirect user after login to his last desired location before login

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -7,10 +7,10 @@ class SessionsController < ApplicationController
   	if user and user.authenticate(params[:session][:password])
   		log_in(user)
       params[:session][:remember_me] == '1' ? remember(user) : forget(user)
-      redirect_to(user)
+      redirect_back_or(user)
   	else
   		flash.now[:warning] = 'invalid email/password combination'
-  		render 'new'
+  		render('new')
   	end
   end
 
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -29,7 +29,7 @@ class UsersController < ApplicationController
       flash[:success] = 'profile updated'
       redirect_to(@user)
     else
-      render 'edit'
+      render('edit')
     end
   end
 
@@ -42,6 +42,7 @@ class UsersController < ApplicationController
 
     def logged_in_user
       unless logged_in?
+        store_location
         flash[:alert] = 'please log in'
         redirect_to(login_url)
       end
