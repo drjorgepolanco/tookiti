8e518df2abbfba78f5baad928d911b5d6e57ddb7
JORGE POLANCO <DR.JORGEPOLANCO@GMAIL.COM>
2015-01-07 18:09:13 -0500

    Add presence and length validations

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,3 +1,6 @@
 class Post < ActiveRecord::Base
   belongs_to :user
+  validates :user_id, presence: true
+  validates :title,   presence: true, length: { maximum: 40, minimum: 3 }
+  validates :image,   presence: true
 end
