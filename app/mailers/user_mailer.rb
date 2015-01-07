9920cc1010e917ff110244b560ccf45bc98d836f
JORGE POLANCO <DR.JORGEPOLANCO@GMAIL.COM>
2015-01-07 03:17:50 -0500

    Develop password_reset(user) for UserMailer

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -3,12 +3,11 @@ class UserMailer < ApplicationMailer
 
   def account_activation(user)
     @user = user
-    mail to: user.email, subject: 'TookiTi! Account Activation'
+    mail to: user.email, subject: 'TookiTi! activate your account'
   end
 
-  def password_reset
-    @greeting = "Hi"
-
-    mail to: "to@example.org"
+  def password_reset(user)
+    @user = user
+    mail to: user.email, subject: 'TookiTi! reset your password'
   end
 end
