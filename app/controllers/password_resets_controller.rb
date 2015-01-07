9a440bde1b1cff0f1164e2a3df6e2ade30ef579f
JORGE POLANCO <DR.JORGEPOLANCO@GMAIL.COM>
2015-01-07 02:59:05 -0500

    Create method create() in PasswordResetsController

--- app/controllers/password_resets_controller.rb
+++ app/controllers/password_resets_controller.rb
@@ -2,6 +2,19 @@ class PasswordResetsController < ApplicationController
   def new
   end
 
+  def create
+  	@user = User.find_by(email: params[:password_reset][:email].downcase)
+  	if @user
+  		@user.create_reset_digest
+  		@user.send_password_reset_email
+  		flash[:info] =  'check your email for password reset instructions'
+  		redirect_to(root_url)
+  	else
+  		flash.now[:warning] = 'we could not find your email address'
+  		render('new')
+  	end
+  end
+
   def edit
   end
 end
