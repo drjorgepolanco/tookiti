1d41146892bd9f56511b79b9da4ffac0f520489f
JORGE POLANCO <DR.JORGEPOLANCO@GMAIL.COM>
2015-01-07 18:22:28 -0500

    Add default scope to post

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,5 +1,6 @@
 class Post < ActiveRecord::Base
   belongs_to :user
+  default_scope -> { order(created_at: :desc) }
   validates :user_id, presence: true
   validates :title,   presence: true, length: { maximum: 40, minimum: 3 }
   validates :image,   presence: true
