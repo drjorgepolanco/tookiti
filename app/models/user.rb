ea5281d6fff94111e10e6361e3450e9259ff5ea4
JORGE POLANCO <DR.JORGEPOLANCO@GMAIL.COM>
2015-01-07 03:09:25 -0500

    Create methods create_reset_digest() and send_password_reset_email()

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,5 @@
 class User < ActiveRecord::Base
-	attr_accessor :remember_token, :activation_token
+	attr_accessor :remember_token, :activation_token, :reset_token
 	before_save { email.downcase! }
 	before_save { first_name.capitalize! }
 	before_save { last_name.capitalize! }
@@ -46,6 +46,16 @@ class User < ActiveRecord::Base
 
 	def send_activation_email
 		UserMailer.account_activation(self).deliver_now
+	end
+
+	def create_reset_digest
+		self.reset_token = User.new_token
+		update_attribute(:reset_digest, User.digest(reset_token))
+		update_attribute(:reset_sent_at, Time.zone.now)
+	end
+
+	def send_password_reset_email
+		UserMailer.password_reset(self).deliver_now
 	end
 
 	private
