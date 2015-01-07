e685d425ca053c51efb072fe355891282a30b933
JORGE POLANCO <DR.JORGEPOLANCO@GMAIL.COM>
2015-01-07 01:50:31 -0500

    Add password_resets routes for new, create, edit and update

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,4 @@
 Rails.application.routes.draw do
-  get 'password_resets/new'
-
-  get 'password_resets/edit'
-
   root                'pages#home'
   get    'about'   => 'pages#about'
   get    'help'    => 'pages#help'
@@ -13,4 +9,5 @@ Rails.application.routes.draw do
   delete 'logout'  => 'sessions#destroy'
   resources :users
   resources :account_activations, only: :edit
+  resources :password_resets,     only: [:new, :create, :edit, :update]
 end
