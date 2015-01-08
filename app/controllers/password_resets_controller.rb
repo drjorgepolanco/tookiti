b78a6a39a32c6e779915f7ea8d65137c99caf017
JORGE POLANCO <DR.JORGEPOLANCO@GMAIL.COM>
2015-01-07 19:01:22 -0500

    Rearrange controllers code to display correctly on git

--- app/controllers/password_resets_controller.rb
+++ app/controllers/password_resets_controller.rb
@@ -7,16 +7,16 @@ class PasswordResetsController < ApplicationController
   end
 
   def create
-  	@user = User.find_by(email: params[:password_reset][:email].downcase)
-  	if @user
-  		@user.create_reset_digest
-  		@user.send_password_reset_email
-  		flash[:info] =  'check your email for password reset instructions'
-  		redirect_to(root_url)
-  	else
-  		flash.now[:warning] = "sorry, we couldn't find your email address"
-  		render('new')
-  	end
+    @user = User.find_by(email: params[:password_reset][:email].downcase)
+    if @user
+      @user.create_reset_digest
+      @user.send_password_reset_email
+      flash[:info] =  'check your email for password reset instructions'
+      redirect_to(root_url)
+    else
+      flash.now[:warning] = "sorry, we couldn't find your email address"
+      render('new')
+    end
   end
 
   def edit
