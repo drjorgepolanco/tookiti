dcf82e127b21240210591f7b2174487bc46de4ec
JORGE POLANCO <DR.JORGEPOLANCO@GMAIL.COM>
2015-01-08 18:33:28 -0500

    Create a integration test for post interface

---
+++ test/integration/post_interface_test.rb
@@ -0,0 +1,37 @@
+require 'test_helper'
+
+class PostInterfaceTest < ActionDispatch::IntegrationTest
+	def setup
+		@user = users(:triculito)
+	end
+
+	test "post interface" do
+		log_in_as(@user)
+		get posts_path
+		assert_select('div.post_thumbnail')
+
+		assert_no_difference('Post.count') do
+			post posts_path, post: { title: '', image: '' }
+		end
+		assert_select('div.alert-box')
+
+		title = 'Lorem Ipsum'
+		image = 'gatito.jpg'
+		assert_difference('Post.count', 1) do
+			post posts_path, post: { title: title, image: image }
+		end
+		assert_redirected_to(posts_path)
+		follow_redirect!
+		assert_match title, response.body
+
+		assert_select('a', text: 'delete')
+		first_post = @user.posts.first
+		assert_difference('Post.count', -1) do
+			delete post_path(first_post)
+		end
+
+		get user_path(users(:jorge))
+		assert_select('a', text: 'delete', count: 0)
+	end
+
+end
