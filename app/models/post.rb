b35ae409bcc76f5a502399627398a80a3f6c9065
JORGE POLANCO <DR.JORGEPOLANCO@GMAIL.COM>
2015-01-08 18:55:19 -0500

    Add image uploader

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,7 +1,8 @@
 class Post < ActiveRecord::Base
   belongs_to :user
   default_scope -> { order(created_at: :desc) }
+  mount_uploader :image, PostImageUploader
   validates :user_id, presence: true
   validates :title,   presence: true, length: { maximum: 40, minimum: 3 }
-  validates :image,   presence: true
+  # validates :image,   presence: true
 end
