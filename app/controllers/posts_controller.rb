932315d0fe489c6f0518460b67ec0fee37d52761
JORGE POLANCO <DR.JORGEPOLANCO@GMAIL.COM>
2015-01-08 17:36:03 -0500

    Update PostsController: update(), destroy(), correct_user()

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,6 +1,7 @@
 class PostsController < ApplicationController
 	before_action :logged_in_user, only: [:create, :edit, :update, :destroy]
 	before_action :set_post,       only: [:show,   :edit, :update, :destroy]
+	before_action :correct_user,   only: [:edit, :update, :destroy]
 
 	def show
 	end
@@ -15,18 +16,11 @@ class PostsController < ApplicationController
 
 	def create
 		@post = current_user.posts.build(post_params)
-		respond_to do |format|
-		  if @post.save
-			  format.html do
-				  flash[:success] = 'your post was succcessfully created'
-				  redirect_to(@post)
-			  end
-			  format.json { render :show, status: :created, location: @post } 
-		  else
-			  format.html { render :new }
-			  format.json { render json: @post.errors,
-			                status: :unprocessable_entity }
-		  end
+		if @post.save
+			flash[:success] = 'your post was succcessfully created'
+			redirect_to(@post)
+		else
+			render('new')
 		end
 	end
 
@@ -34,36 +28,30 @@ class PostsController < ApplicationController
 	end
 
 	def update
-		respond_to do |format|
-			if @post.update(post_params)
-				format.html do
-					flash[:success] = 'your post was succcessfully updated'
-					redirect_to(@post)
-				end
-				format.json { render :show, status: :ok, location: @post }
-			else
-				format.html { render :edit }
-				format.json { render json: @post.errors,
-				              status: :unprocessable_entity }
-			end
+		if @post.update_attributes(post_params)
+			flash[:success] = 'your post was successfully updated'
+			redirect_to(@post)
+		else
+			render('edit')
 		end
 	end
 
 	def destroy
 		@post.destroy
-		respond_to do |format|
-			format.html do
-				flash[:success] = 'your post was successfully deleted'
-				redirect_to(posts_url)
-			end
-			format.json { head :no_content }
-		end
+		flash[:success] = 'your post was successfully deleted'
+		redirect_to request.referrer || @posts
 	end
 
 	private
 
 	  def set_post
 	  	@post = Post.find(params[:id])
+	  end
+
+	  def correct_user
+	  	@post = current_user.posts.find_by(id: params[:id])
+	  	redirect_to root_url if @post.nil?
+	  	flash[:warning] = "you can't update or delete somebody else's posts"
 	  end
 
 	  def post_params
