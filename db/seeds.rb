809f363461b9fbe7370549b6d2ee0fe795bd303c
JORGE POLANCO <DR.JORGEPOLANCO@GMAIL.COM>
2015-01-07 20:31:10 -0500

    Add sample posts to seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,12 +7,17 @@ User.create!(first_name: 'Julito',
 	           activated:  true,
 	           activated_at: Time.zone.now)
 
-		99.times do |n|
-	first_name = Faker::Name.first_name
-	last_name  = Faker::Name.last_name
-	email      = "example-#{n+1}@railstutorial.org"
-	password   = 'password'
-	User.create!(first_name: first_name, last_name: last_name, email: email,
-		           password: password, password_confirmation: password,
-		           activated: true, activated_at: Time.zone.now)
+99.times do |n|
+first_name = Faker::Name.first_name
+last_name  = Faker::Name.last_name
+email      = "example-#{n+1}@railstutorial.org"
+password   = 'password'
+User.create!(first_name: first_name, last_name: last_name, email: email,
+	           password: password, password_confirmation: password,
+	           activated: true, activated_at: Time.zone.now)
+
+users = User.order(:created_at).take(6)
+50.times do
+	title = Faker::Lorem.sentence(2)
+	image = 'http://funny-pics-fun.com/wp-content/uploads/Very-Funny-Animal-Faces.jpg'
 end
