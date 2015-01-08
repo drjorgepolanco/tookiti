0aa59b099601b77f5bc0e38a04723afeeb3ac285
JORGE POLANCO <DR.JORGEPOLANCO@GMAIL.COM>
2015-01-08 12:27:51 -0500

    Create PostsController: 7 golden plus set_post() and post_params(). Still not finished.

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,2 +1,43 @@
 class PostsController < ApplicationController
+	before_action :logged_in_user, only: [:create, :edit, :update, :destroy]
+	before_action :set_post,       only: [:show]
+
+	def show
+	end
+
+	def index
+	end
+
+	def new
+		@post = Post.new
+	end
+
+	def create
+		@post = current_user.posts.build(post_params)
+		if @post.save
+			flash[:success] = 'post created!'
+			redirect_to(root_url)
+		else
+			render('posts/new')
+		end
+	end
+
+	def edit
+	end
+
+	def update
+	end
+
+	def destroy
+	end
+
+	private
+
+	  def set_post
+	  	@post = Post.find(params[:id])
+	  end
+
+	  def post_params
+	  	params.require(:post).permit(:title, :image)
+	  end
 end
