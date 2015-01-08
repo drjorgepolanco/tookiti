f258f307a4532a80c5cdad0fbe3f4f1d4ca15a40
JORGE POLANCO <DR.JORGEPOLANCO@GMAIL.COM>
2015-01-08 14:29:30 -0500

    Test that only the creator of a post can update or delete it

--- test/controllers/posts_controller_test.rb
+++ test/controllers/posts_controller_test.rb
@@ -3,7 +3,8 @@ require 'test_helper'
 class PostsControllerTest < ActionController::TestCase
 
 	def setup
-		@post = posts(:lemur)
+		@post       = posts(:lemur)
+		@other_user = users(:jorge)
 	end
 
 	test "should redirect create when not logged in" do
@@ -13,6 +14,34 @@ class PostsControllerTest < ActionController::TestCase
 		assert_redirected_to(login_url)
 	end
 
+	test "should redirect edit when not logged in" do
+		get :edit, id: @post
+		assert_not flash.empty?
+		assert_redirected_to(login_url)
+	end
+
+	test "should redirect update when not logged in" do
+		patch :update, id: @post, post: { title: @post.title,
+		                                  image: @post.image }
+		assert_not flash.empty?
+		assert_redirected_to(login_url)
+	end
+
+	test "should redirect edit when logged in as wrong user" do
+		log_in_as(@other_user)
+		get :edit, id: @post
+		assert flash.empty?
+		assert_redirected_to(root_url)
+	end
+
+	test "should redirect update when logged in as a wrong user" do
+		log_in_as(@other_user)
+		patch :update, id: @post, post: { title: @post.title,
+		                                  image: @post.image }
+		assert flash.empty?
+		assert_redirected_to(root_url)
+	end
+	
 	test "should redirect destroy when not logged in" do
 		assert_no_difference('Post.count') do
 			delete :destroy, id: @post
