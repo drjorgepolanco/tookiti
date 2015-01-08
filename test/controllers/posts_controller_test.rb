4c1f7c10ecd9f9879be1ea2a1b777cfd3fdae748
JORGE POLANCO <DR.JORGEPOLANCO@GMAIL.COM>
2015-01-08 10:42:36 -0500

    Test that user must be logged in before creating or deleting a post

--- test/controllers/posts_controller_test.rb
+++ test/controllers/posts_controller_test.rb
@@ -1,7 +1,22 @@
 require 'test_helper'
 
 class PostsControllerTest < ActionController::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+
+	def setup
+		@post = posts(:lemur)
+	end
+
+	test "should redirect create when not logged in" do
+		assert_no_difference('Post.count') do
+			post :create, post: { title: 'The title', image: 'lemur.jpg'}
+		end
+		assert_redirected_to(login_url)
+	end
+
+	test "should redirect destroy when not logged in" do
+		assert_no_difference('Post.count') do
+			delete :destroy, id: @post
+		end
+		assert_redirected_to(login_url)
+	end
 end
