11f4bab7efc8f2260ff326938b95f3d4b60e3660
JORGE POLANCO <DR.JORGEPOLANCO@GMAIL.COM>
2015-01-09 16:34:20 -0500

    Add contests and relations to seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -23,3 +23,17 @@ users = User.order(:created_at).take(6)
 	image = File.open(File.join(Rails.root, 'test/fixtures/files/krake.jpg'))
 	users.each { |user| user.posts.create!(title: title, image: image) }
 end
+
+users = User.order(:created_at).take(6)
+15.times do
+	title  = "#{Faker::Lorem.word} #{Faker::Lorem.word} #{Faker::Lorem.word}"
+	poster = 'http://media-cache-ec0.pinimg.com/736x/1c/08/20/1c08202b325a1058b0e3d1bf8636dc51.jpg'
+	users.each { |user| user.contests.create!(title: title, poster: poster) }
+end
+
+users = User.all
+user  = users.first
+following = users[2..50]
+followers = users[3..40]
+following.each { |followed| user.follow(followed) }
+followers.each { |follower| follower.follow(user) }
