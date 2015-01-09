70c1512870858fb04c38bc3cb6213787781fa438
JORGE POLANCO <DR.JORGEPOLANCO@GMAIL.COM>
2015-01-09 15:04:31 -0500

    Generate Relationship model

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,19 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150107223243) do
+ActiveRecord::Schema.define(version: 20150109195232) do
+
+  create_table "contests", force: true do |t|
+    t.string   "title"
+    t.string   "poster"
+    t.text     "description"
+    t.integer  "user_id"
+    t.boolean  "poll",        default: false
+    t.datetime "created_at",                  null: false
+    t.datetime "updated_at",                  null: false
+  end
+
+  add_index "contests", ["user_id"], name: "index_contests_on_user_id"
 
   create_table "posts", force: true do |t|
     t.integer  "user_id"
@@ -23,6 +35,17 @@ ActiveRecord::Schema.define(version: 20150107223243) do
 
   add_index "posts", ["user_id", "created_at"], name: "index_posts_on_user_id_and_created_at"
   add_index "posts", ["user_id"], name: "index_posts_on_user_id"
+
+  create_table "relationships", force: true do |t|
+    t.integer  "follower_id"
+    t.integer  "followed_id"
+    t.datetime "created_at",  null: false
+    t.datetime "updated_at",  null: false
+  end
+
+  add_index "relationships", ["followed_id"], name: "index_relationships_on_followed_id"
+  add_index "relationships", ["follower_id", "followed_id"], name: "index_relationships_on_follower_id_and_followed_id", unique: true
+  add_index "relationships", ["follower_id"], name: "index_relationships_on_follower_id"
 
   create_table "users", force: true do |t|
     t.string   "first_name"
