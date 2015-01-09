e3e86e6a544853b089076816e32531b3027ddc7e
JORGE POLANCO <DR.JORGEPOLANCO@GMAIL.COM>
2015-01-09 00:57:30 -0500

    Add attribute validations and default scope to Contest model

--- app/models/contest.rb
+++ app/models/contest.rb
@@ -1,4 +1,8 @@
 class Contest < ActiveRecord::Base
   belongs_to :user
   has_many   :posts
+  default_scope -> { order(created_at: :desc) }
+  validates  :user_id, presence: true
+  validates  :title,  presence: true, length: { minimum: 3, maximum: 30 }
+  validates  :poster, presence: true
 end
