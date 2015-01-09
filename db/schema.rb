018c8f972818ad9dcd025707a4b2a30a545b1634
JORGE POLANCO <DR.JORGEPOLANCO@GMAIL.COM>
2015-01-09 15:20:22 -0500

    Destroy model Relationship

--- db/schema.rb
+++ db/schema.rb
@@ -11,8 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-
-ActiveRecord::Schema.define(version: 20150109195232) do
+ActiveRecord::Schema.define(version: 20150109045430) do
 
   create_table "contests", force: true do |t|
     t.string   "title"
@@ -36,17 +35,6 @@ ActiveRecord::Schema.define(version: 20150109195232) do
 
   add_index "posts", ["user_id", "created_at"], name: "index_posts_on_user_id_and_created_at"
   add_index "posts", ["user_id"], name: "index_posts_on_user_id"
-
-  create_table "relationships", force: true do |t|
-    t.integer  "follower_id"
-    t.integer  "followed_id"
-    t.datetime "created_at",  null: false
-    t.datetime "updated_at",  null: false
-  end
-
-  add_index "relationships", ["followed_id"], name: "index_relationships_on_followed_id"
-  add_index "relationships", ["follower_id", "followed_id"], name: "index_relationships_on_follower_id_and_followed_id", unique: true
-  add_index "relationships", ["follower_id"], name: "index_relationships_on_follower_id"
 
   create_table "users", force: true do |t|
     t.string   "first_name"
