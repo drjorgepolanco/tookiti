45db6b2104fca7831378f3041d633678a747f36d
JORGE POLANCO <DR.JORGEPOLANCO@GMAIL.COM>
2015-01-09 00:13:28 -0500

    Relocate correct_user() to ApplicationController to use it in other controllers

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,11 +2,21 @@ class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
   include SessionsHelper
 
+  private
+
   def logged_in_user
   	unless logged_in?
   		store_location
   		flash[:warning] = 'please, log in'
   		redirect_to(login_url)
+  	end
+  end
+
+  def correct_user
+  	@post = current_user.posts.find_by(id: params[:id])
+  	if @post.nil?
+  		redirect_to root_url 
+  		flash[:warning] = "you can't update or delete somebody else's stuff"
   	end
   end
   
