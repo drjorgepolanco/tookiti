048546b7ad8e0aabd754165a1b0b9a2bfc82358d
JORGE POLANCO <DR.JORGEPOLANCO@GMAIL.COM>
2015-01-09 16:04:44 -0500

    Add has_many followers and passive_relationships to User

--- app/models/user.rb
+++ app/models/user.rb
@@ -11,10 +11,14 @@ class User < ActiveRecord::Base
 	has_secure_password
 	has_many :posts,    dependent: :destroy
 	has_many :contests, dependent: :destroy
-	has_many :active_relationships, class_name: "Relationship",
-	                                foreign_key: "follower_id",
-	                                dependent: :destroy
+	has_many :active_relationships,  class_name: "Relationship",
+	                                 foreign_key: "follower_id",
+	                                 dependent: :destroy
+	has_many :passive_relationships, class_name: "Relationship",
+	                                 foreign_key: "followed_id",
+	                                 dependent: :destroy
 	has_many :following, through: :active_relationships, source: :followed
+	has_many :followers, through: :passive_relationships
 
 	VALID_EMAIL = /\A[\w+\-.]+@[a-z\d\-]+(\.[a-z\d\-]+)*\.[a-z]+\z/i
 	validates :password, length: { minimum: 8 }, allow_blank: true
