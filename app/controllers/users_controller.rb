beb8db32959bec780763f2bff4b27248c138ce07
JORGE POLANCO <DR.JORGEPOLANCO@GMAIL.COM>
2015-01-09 23:55:46 -0500

    Add methods following() and followers() to UsersController

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,6 @@
 class UsersController < ApplicationController
-  before_action :logged_in_user, only: [:index, :edit, :update, :destroy]
+  before_action :logged_in_user, only: [:index, :edit, :update, :destroy
+                                        :following, :followers]
   before_action :set_user,       only: [:show, :destroy]
   before_action :correct_user,   only: [:edit, :update]
   before_action :admin_user,     only: :destroy
@@ -44,6 +45,20 @@ class UsersController < ApplicationController
     @user.destroy
     flash[:success] = 'user deleted'
     redirect_to(users_url)
+  end
+
+  def following
+    @title = 'following'
+    @user  = User.find(params[:id])
+    @users = @user.following
+    render('show_follow')
+  end
+
+  def followers
+    @title = 'followers'
+    @user  = User.find(params[:id])
+    @users = @user.followers
+    render('show_follow')
   end
 
   private
